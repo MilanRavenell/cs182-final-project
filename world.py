3fb4de14a2f1ea5e87270b9d38eeb878d975ccb5
Milan Ravenell <Milan Ravenell>
2016-11-29 21:11:33 -0500

    first

---
+++ world.py
@@ -0,0 +1,13 @@
+WIDTH = 3
+HEIGHT = 3
+
+class WorldStateData:
+	def __init__(self, prev = None):
+		self.taxiLocation = None
+		self.passenger_
+		self.score = 0
+
+		if not prev = None
+
+class World:
+	def __init__(self):
