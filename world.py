4e91b520e5910191cee1a2837664e890b087ca1f
Milan Ravenell <Milan Ravenell>
2016-11-29 22:07:34 -0500

    state

--- world.py
+++ world.py
@@ -1,13 +1,42 @@
 WIDTH = 3
 HEIGHT = 3
 
+class Action:
+	NORTH = 'North'
+	SOUTH = 'South'
+	EAST = 'East'
+	WEST = 'West'
+	PICK = 'Pick'
+	DROP = 'Drop'
+
+	actions = {NORTH: (0, 1),
+               SOUTH: (0, -1),
+               EAST:  (1, 0),
+               WEST:  (-1, 0),
+               PICK:  (0, 0),
+               DROP:  (0, 0)}
+
+    def directionToVector(direction):
+        dx, dy =  Action.actions[action]
+        return (dx, dy)
+
+
 class WorldStateData:
 	def __init__(self, prev = None):
 		self.taxiLocation = None
-		self.passenger_
+
 		self.score = 0
 
 		if not prev = None
 
-class World:
+class State:
 	def __init__(self):
+		self.taxiLocation = None
+		self.has_passenger = False
+		self.destination = None
+
+	#def passanger_at_location(x,y):
+
+
+class World:
+	self.passenger_distribution = {}
