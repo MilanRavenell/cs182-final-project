ed025d795a8a87fb8ca58096c1051483bc57d3a5
mandelpatrick16 <mandelapatrick@college.harvard.edu>
2016-12-01 02:12:56 -0500

    wth

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from world import World
 from qlearningAgents import TaxiAgent 
+from graphicsGridWorld import GraphicsGridworldDisplay
 
 def main():
 	taxi = TaxiAgent()
 	world = World(taxi)
-	for i in range(10):
-		world.run()
-		print world.getFavoredProportion()
+    # for i in range(10):
+	#	world.run()
+	#	print world.getFavoredProportion()
 	# for i in taxi.qvalues.keys():
 	# 	print str(i) +': ' + str(taxi.qvalues[i])
-
+    
 
 main()
